3c4b494556fa3a38af2d2b111410135dab6410e3
usergithub-1 <Myname-25@hotmail.com>
2020-11-23 14:10:36 +0700

    update

--- routes/SEG.js
+++ routes/SEG.js
@@ -1,38 +1,30 @@
+
 const express = require('express');
 const router = express.Router();
 const User = require('../model/user');
 const Post4selves = require('../model/post4selves');
 const Commento = require('../model/comment');
 var passport = require('passport');
-var passportLocal= require('passport-local');
-var passportLocalMongoose=require('passport-local-mongoose');
+var passportLocal = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
 
 
-router.get("/",function(req,res){
-        Post4selves.find({name : req.query.name},function(err,post4selves){
-            if(err){
-                console.log('asdasdas');
-            }
-            if(!post4selves.length){
-                
-                res.redirect('back');
+router.get("/", function (req, res) {
+  Post4selves.find({ name: req.query.name }, function (err, post4selves) {
+    if (err) { console.log('asdasdas'); }
+    else if (!post4selves.length) { res.redirect('back'); }
+    else {
+      Commento.find({}, function (error, comment) {
+        if (error) { console.log(error); }
+        else {
+          User.find({}, function (err, user) {
+            if (err) { console.log(err) }
+            else { res.render('public', { Post4selves: post4selves, Commento: comment, User: user }) }
+          })
+        }
+      });
+    }
+  })
+})
 
-            }
-            else {
-            Commento.find({},function(error,comment){
-                if(error){
-                  console.log(error);
-                }else {
-                    User.find({},function(err,user){
-                      if(err) {console.log(err)}
-                      else{
-                       
-                         res.render('public',{Post4selves:post4selves,Commento:comment,User:user})
-                      }
-                    })
-                }
-              });
-            }
-        })
-})  
-  module.exports = router;
+module.exports = router;
--- routes/editpc.js
+++ routes/editpc.js
@@ -7,65 +7,42 @@ const Post4selves = require('../model/post4selves');
 const Commento = require('../model/comment');
 const middleware = require('../middleware');
 var passport = require('passport');
-var passportLocal= require('passport-local');
-var passportLocalMongoose=require('passport-local-mongoose');
+var passportLocal = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
 
+//post 
+router.get("/:id", middleware.isLoggedIn, function (req, res) {
+  console.log(req.params.id);
+  Post4selves.findById(req.params.id, function (err, found) {
+    if (err) { console.log(err) }
+    else {
+      console.log(found);
+      res.render("editpost", { post4selves: found });
+    }
+  });
+});
 
-
-  
-  
-  //post 
-  router.get("/:id",middleware.isLoggedIn,function(req,res){
-      console.log(req.params.id);
-    Post4selves.findById(req.params.id,function(err,found){
-      if(err){
-          console.log(err)
-          
+router.put("/:id", function (req, res) {
+  Post4selves.findByIdAndUpdate(req.params.id, req.body.post4selves, function (err, update) {
+    if (err) { console.log(err) }
+    else {
+      Commento.findByIdAndUpdate(req.params.id, req.body.comment, function (err, updateto) {
+        if (err) { console.log(err) }
+        else {
+          console.log("456");
+          res.redirect('/users/public');
         }
-      else{
-        console.log(found);
-      res.render("editpost",{post4selves:found});
-      }
-    });
+      });
+    }
   });
-  
-  router.put("/:id",function(req,res){
-    Post4selves.findByIdAndUpdate(req.params.id,req.body.post4selves,function(err,update){
-      if(err) {console.log(err)}
-      else {
-        Commento.findByIdAndUpdate(req.params.id,req.body.comment,function(err,updateto){
-          if(err) {console.log(err)}
-          else {
-            console.log("456");
-            res.redirect('/users/public');
-          }
-        });
-      }
-    });
+});
+
+// comment
+router.get("/editcomment/:id", function (req, res) {
+  Commento.findById(req.params.id, function (err, find) {
+    if (err) { console.log(err) }
+    else { res.render("editcomment", { comment: find }); }
   });
-  
-
-  
-  // comment
-  router.get("/editcomment/:id",function(req,res){
-    Commento.findById(req.params.id,function(err,find){
-      if(err) {console.log(err)}
-      else{
-        res.render("editcomment",{comment:find});
-      }
-    });
-  });
-  
-  
-  
-
-
-
-
-
-
-
-
-
+});
 
 module.exports = router;
